547bf68516f81b139b1721a01b1b05cde37a0c65
MyCakeAleks <kchroline@gmail.com>
2026-04-24 18:51:56 +0300

    fix: add trustHost: true to next-auth config for production

--- auth.ts
+++ auth.ts
@@ -3,6 +3,7 @@ import Google from 'next-auth/providers/google'
 import Apple from 'next-auth/providers/apple'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost: true,
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID ?? '',
